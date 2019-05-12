1132724bf407e885a6888d680544ddde196e3917
Ariana Hall <amhall4323@ung.edu>
2019-05-12 22:14:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string) {
-  console.log("HELLO!" === string)
+  console.log(string.toUpperCase())
 }
 function logWhisper(string) {
-  console.log('hello!' === string)
+  console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {

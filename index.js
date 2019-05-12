042f9c152269425a27df5902aef0a99f15da9154
Ariana Hall <amhall4323@ung.edu>
2019-05-12 22:06:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,10 +21,10 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string.toLowerCase === string) {
     return "I can't hear you!"
-  }  
+  }
   if (string.toUpperCase === string) {
     return "YES INDEED!"
-  } 
+  }
   if (string.mixedCase === string) {
     return "I love you, too."
   }

aab8614671c3a0a87e95a08f3c55ea40166e1a33
Ariana Hall <amhall4323@ung.edu>
2019-05-12 21:49:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase)
 }
 function sayHiToGrandma(string) {
-  if string === uppercase {
+  if 'hello' === uppercase {
     return "YES INDEED!"
   } else string === lowercase {
     return "I can't hear you!"
